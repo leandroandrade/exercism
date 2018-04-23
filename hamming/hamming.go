2eead033d5eba361fdb03e612d3494ef0ed560ee
Leandro Andrade <leandro.andrade>
2018-04-23 14:46:21 -0300

    exercise hamming

---
+++ hamming/hamming.go
@@ -0,0 +1,31 @@
+package hamming
+
+import (
+	"strings"
+	"errors"
+)
+
+func processHamming(s1, s2 string) int {
+	var total int
+
+	s1Split := strings.Split(s1, "")
+	s2Split := strings.Split(s2, "")
+
+	for i := 0; i < len(s1); i++ {
+		if s1Split[i] != s2Split[i] {
+			total++
+		}
+	}
+	return total
+}
+
+func Distance(s1, s2 string) (int, error) {
+	if s1 == "" && s2 == "" || s1 == s2 {
+		return 0, nil
+	}
+
+	if len(s1) != len(s2) {
+		return -1, errors.New("nucleotides are different")
+	}
+	return processHamming(s1, s2), nil
+}
